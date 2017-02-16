fd9e79ab892b91214dfc59d94a817b291c18f04f
Gareth Jones <garethmichaeljones@gmail.com>
2017-02-16 17:20:53 +0000

    Handle school user's rejection of OAuth request in a nice way.

--- oauth-sample.rb
+++ oauth-sample.rb
@@ -6,41 +6,50 @@ require 'sinatra'
 
 include ERB::Util
 
-APP_ID     = ENV['APP_ID']
-APP_SECRET = ENV['APP_SECRET']
-CALLBACK   = url_encode('http://localhost:4567/auth/assembly/callback')
+use Rack::Session::Pool, :expire_after => 10000
 
-get '/' do 
-	erb :index	
+APP_ID       = ENV['APP_ID']
+APP_SECRET   = ENV['APP_SECRET']
+CALLBACK     = url_encode('http://localhost:4567/auth/assembly/callback')
+PLATFORM_URI = 'https://platform-sandbox.assembly.education/'
+
+get '/' do
+  # You should implement your own logic to check whether a school has paid for your app before allowing them to authorize
+  # because Assembly charge on a per-authorization basis.
+  redirect to('/unpaid') if params['school_urn'] == 'XXXXXX'
+  session['school_urn'] = params['school_urn']
+  erb :index
 end
 
 get '/auth_me' do
-	state  = (0...50).map { ('a'..'z').to_a[rand(26)] }.join
-	scopes = %w(students teaching_groups).join('+')
-	redirect to("https://platform-sandbox.assembly.education/oauth/authorize?redirect_uri=#{CALLBACK}&client_id=#{APP_ID}&scope=#{scopes}&state=#{state}")
+  state  = (0...50).map { ('a'..'z').to_a[rand(26)] }.join
+  scopes = %w(students teaching_groups).join('+')
+  redirect to("#{PLATFORM_URI}/oauth/authorize?urn=#{session['school_urn']}&redirect_uri=#{CALLBACK}&client_id=#{APP_ID}&scope=#{scopes}&state=#{state}")
 end
 
 get '/auth/assembly/callback' do
-  logger.info "Callback Params: #{params}"
-
-	auth_code = params['code']    # This code may be exchanged for an access and refresh token scoped for only the authorising school.
-	csrf_code = params['state']   # In your real app you should verify this code matches what you sent.
+  auth_code = params['code'] # This code may be exchanged for an access and refresh token scoped for only the authorising school.
+  csrf_code = params['state'] # In your real app you should verify this code matches what you sent.
   error     = params['error']
-	error_msg = params['error_description']
+  error_msg = params['error_description']
 
   if error
-		erb :rejected, locals: { error: error, message: error_msg }
+    erb :rejected, locals: { error: error, message: error_msg }
   else
-		response = RestClient.post "https://platform-sandbox.assembly.education/oauth/token?grant_type=authorization_code&code=#{auth_code}&redirect_uri=#{CALLBACK}", {},
-			{ Accept: 'application/json', Authorization: basic_auth_header(APP_ID, APP_SECRET) }
+    response = RestClient.post "#{PLATFORM_URI}/oauth/token?grant_type=authorization_code&code=#{auth_code}&redirect_uri=#{CALLBACK}", {},
+      { Accept: 'application/json', Authorization: basic_auth_header(APP_ID, APP_SECRET) }
 
-		json = JSON.parse(response.body)
-		erb :authed, locals: { json: json }
+    json = JSON.parse(response.body)
+    erb :authed, locals: { json: json }
   end
 end
 
+get '/unpaid' do
+  erb :unpaid
+end
+
 def basic_auth_header(id, secret)
-	'Basic ' + Base64.strict_encode64("#{id}:#{secret}")
+  'Basic ' + Base64.strict_encode64("#{id}:#{secret}")
 end
 
 
