2ef552ba7d696f3c937f61d932e469a69e5e0dea
Gareth Jones <garethmichaeljones@gmail.com>
2017-02-16 07:43:51 +0000

    Handles a user rejection of OAuth request.

--- oauth-sample.rb
+++ oauth-sample.rb
@@ -21,14 +21,22 @@ get '/auth_me' do
 end
 
 get '/auth/assembly/callback' do
+  logger.info "Callback Params: #{params}"
+
 	auth_code = params['code']    # This code may be exchanged for an access and refresh token scoped for only the authorising school.
 	csrf_code = params['state']   # In your real app you should verify this code matches what you sent.
+  error     = params['error']
+	error_msg = params['error_description']
 
-	response = RestClient.post "https://platform-sandbox.assembly.education/oauth/token?grant_type=authorization_code&code=#{auth_code}&redirect_uri=#{CALLBACK}", {},
-		{ Accept: 'application/json', Authorization: basic_auth_header(APP_ID, APP_SECRET) }
+  if error
+		erb :rejected, locals: { error: error, message: error_msg }
+  else
+		response = RestClient.post "https://platform-sandbox.assembly.education/oauth/token?grant_type=authorization_code&code=#{auth_code}&redirect_uri=#{CALLBACK}", {},
+			{ Accept: 'application/json', Authorization: basic_auth_header(APP_ID, APP_SECRET) }
 
-	json = JSON.parse(response.body)
-	erb :authed, locals: { json: json }
+		json = JSON.parse(response.body)
+		erb :authed, locals: { json: json }
+  end
 end
 
 def basic_auth_header(id, secret)
